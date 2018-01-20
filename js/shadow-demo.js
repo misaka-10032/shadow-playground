2543634417e230fe6575e9cd778a3d8b7cc8c30e
Rocky Cai <longqic@google.com>
2018-01-19 17:44:08 -0800

    remove debugging code

--- js/shadow-demo.js
+++ js/shadow-demo.js
@@ -53,11 +53,7 @@ varying vec4 vLightCoord;
 
 void main() {
   vec2 lightCoord = vLightCoord.xy / vLightCoord.w;
-  float z = texture2D(uDepthMap, lightCoord).z;
-  // gl_FragColor = vec4(z, z, z, 1);
-  // gl_FragColor = vec4(lightCoord, 0, 1);
-  gl_FragColor = vec4(vLightCoord.zzz / vLightCoord.w, 1);
-  // gl_FragColor = vec4(texture2D(uDepthMap, lightCoord).zzz, 1);
+  gl_FragColor = vec4(texture2D(uDepthMap, lightCoord).zzz, 1);
   // gl_FragColor = vec4(vRGBx.xyz, 1);
 }
 
@@ -162,12 +158,11 @@ define(["glm", "glh"], function(glm, glh) {
       gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, pass.indexBuffer);
       
       // Draw floor.
-      // TODO
-//      gl.bindBuffer(gl.ARRAY_BUFFER, pass.floorPositionBuffer);
-//      gl.vertexAttribPointer(
-//          pass.positionLocation, kFloorPositionDim, gl.FLOAT, /* normalize= */ false,
-//          kDefaultStride, kDefaultOffset);
-//      gl.drawElements(gl.TRIANGLES, kCubeIndexCount, gl.UNSIGNED_SHORT, kDefaultOffset);
+      gl.bindBuffer(gl.ARRAY_BUFFER, pass.floorPositionBuffer);
+      gl.vertexAttribPointer(
+          pass.positionLocation, kFloorPositionDim, gl.FLOAT, /* normalize= */ false,
+          kDefaultStride, kDefaultOffset);
+      gl.drawElements(gl.TRIANGLES, kCubeIndexCount, gl.UNSIGNED_SHORT, kDefaultOffset);
       
       // Draw cube.
       gl.bindBuffer(gl.ARRAY_BUFFER, pass.cubePositionBuffer);
@@ -235,42 +230,7 @@ define(["glm", "glh"], function(glm, glh) {
     gl.enable(gl.DEPTH_TEST);
     initPasses(this, gl);
     const texture = this.depthPass.draw();
-    // TODO
-    // this.shadowPass.draw(texture);
-    
-    var _vertices = [-1, -1, -1, 1, 1, 1, 1, -1];
-    var _indices = [0, 1, 2, 0, 2, 3];
-    var _vbuffer = glh.createFloatVertexBuffer(gl, _vertices);
-    var _ibuffer = glh.createShortIndexBuffer(gl, _indices);
-    var _vshader = `
-      attribute vec2 aPos;
-      void main() {
-        gl_Position = vec4(aPos, 0, 1);
-      }`;
-    var _fshader = `
-      precision mediump float;
-      uniform sampler2D uTex;
-      void main() {
-        //gl_FragColor = vec4(1, 0, 0, 1);
-        vec2 uv = gl_FragCoord.xy / vec2(400, 300);
-        gl_FragColor = texture2D(uTex, uv);
-      }`;
-    var _program = glh.initShaderProgram(gl, _vshader, _fshader);
-    var _posLocation = gl.getAttribLocation(_program, "aPos");
-    var _texLocation = gl.getUniformLocation(_program, "uTex");
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    gl.useProgram(_program);
-    gl.enableVertexAttribArray(_posLocation);
-    const _textureUnit = 1;
-    gl.activeTexture(gl.TEXTURE0 + _textureUnit);
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-    gl.uniform1i(_texLocation, _textureUnit);
-    gl.bindBuffer(gl.ARRAY_BUFFER, _vbuffer);
-    gl.vertexAttribPointer(
-        _posLocation, /* dim= */ 2, gl.FLOAT, /* normalize= */ false,
-        kDefaultStride, kDefaultOffset);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, _ibuffer);
-    gl.drawElements(gl.TRIANGLES, _indices.length, gl.UNSIGNED_SHORT, kDefaultOffset);
+    this.shadowPass.draw(texture);
   }
   
   function onResize() {
