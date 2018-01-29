4b4986073801f7fcc1a6427797081478c0514a45
Rocky Cai <longqic@google.com>
2018-01-28 18:14:29 -0800

    Apply mipmap in var shadow map.

--- js/glh.js
+++ js/glh.js
@@ -66,6 +66,7 @@ define([], function() {
       gl.TEXTURE_2D, /* level= */ 0, /* internalFormat= */ gl.RGBA32F, width, height,
       /* border= */ 0, /* format= */ gl.RGBA, /* type= */ gl.FLOAT, data);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
     gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
     gl.bindTexture(gl.TEXTURE_2D, null);
